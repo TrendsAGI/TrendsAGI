0b6202fb5f91e761c86268c52e6bdd353b7703f6
TrendsAGI <contact@trendsagi.com>
2026-04-16 19:04:55 +0100

    chore: remove api client and agent/context surfaces

--- src/trendsagi/__init__.py
+++ src/trendsagi/__init__.py
@@ -1,7 +1,6 @@
-from .client import TrendsAGIClient
-from . import exceptions
 from importlib.metadata import PackageNotFoundError, version
 
+from . import exceptions
 from .integrations import (
     AdPlatformExecutor,
     ExecutionResult,
@@ -11,6 +10,7 @@ from .integrations import (
     TikTokAdsExecutor,
     LinkedInAdsExecutor,
 )
+from .models import AIInsight, AudienceInjection
 
 try:
     __version__ = version("trendsagi")
@@ -18,9 +18,10 @@ except PackageNotFoundError:
     __version__ = "0.0.0"
 
 __all__ = [
-    "TrendsAGIClient",
     "__version__",
     "exceptions",
+    "AIInsight",
+    "AudienceInjection",
     "AdPlatformExecutor",
     "ExecutionResult",
     "PlatformExecutionError",
